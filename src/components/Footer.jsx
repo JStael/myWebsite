fe0fb029fdb9f12efc15ea8d72fbefd8bfa96a74
João Stael <joaostael@gmail.com>
2021-09-25 19:30:29 -0300

    Ajustes no header, main e footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,37 @@
 import Link from "next/link";
 import nextjsLight from "../../public/nextjs_light.svg";
+import nextjsDark from "../../public/nextjs_dark.svg";
 import Image from "next/image";
 import styles from "../../styles/Footer.module.scss";
+import { useContext, useEffect, useState } from "react";
+import { GlobalContext } from "../provider/GlobalContext";
 
 const Footer = () => {
+  const context = useContext(GlobalContext);
+
+  const [appTheme, setAppTheme] = useState();
+
+  let theme = appTheme == "light" ? `${styles["light"]}` : `${styles["dark"]}`;
+
+  useEffect(() => {
+    setAppTheme(context.globalTheme);
+  }, [context.globalTheme]);
+
   return (
-    <footer className={styles.footer}>
-      <p>
-        &copy; {new Date().getFullYear()} | Desenvolvido com&nbsp;
-        <Link href={"https://nextjs.org/"}>
-          <a target="_blank">
-            <Image src={nextjsLight} alt="Logo NextJs" />
-          </a>
-        </Link>
-      </p>
+    <footer className={theme}>
+      <div className={styles.footer}>
+        <p>
+          &copy; {new Date().getFullYear()} | Desenvolvido com&nbsp;&nbsp;
+          <Link href={"https://nextjs.org/"}>
+            <a target="_blank">
+              <Image
+                src={appTheme == "light" ? nextjsLight : nextjsDark}
+                alt="Logo NextJs"
+              />
+            </a>
+          </Link>
+        </p>
+      </div>
     </footer>
   );
 };
